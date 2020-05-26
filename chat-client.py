a8099c19c9d33757acb83a00ba97276f1c4e30be
Peer-Lucas Jeske <jeskep@th-brandenburg.de>
2020-05-26 15:14:53 +0200

    initial commit

---
+++ chat-client.py
@@ -0,0 +1,114 @@
+import socket
+import threading
+import pyaudio
+import random
+import time
+import tkinter
+import queue
+from emotions import EmotionDetection
+
+
+class GuiPart:
+    def __init__(self, master, message_queue):
+        self.queue = message_queue
+        # Set up the GUI
+        self.emotion_label = tkinter.Label(master, text="NEUTRAL", foreground="white", font=("Courier", 44),
+                                           background="black", width=300, height=100)
+        self.emotion_label.pack()
+        # Add more GUI stuff here depending on your specific needs
+
+    def process_incoming(self):
+        """Handle all messages currently in the queue, if any."""
+        while self.queue.qsize():
+            try:
+                msg = self.queue.get(0)
+                self.emotion_label.config(text=msg)
+                self.emotion_label.update()
+            except Exception as e:
+                print(e)
+
+
+class Client:
+    def __init__(self, master, host):
+        self.master = master
+        self.queue = queue.Queue()
+        self.gui = GuiPart(master, self.queue)
+        self.emotion_detection = EmotionDetection()
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s_text = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        while 1:
+            try:
+                self.target_ip = host
+                self.target_port = 9999
+                self.target_text_port = 9998
+
+                self.s.connect((self.target_ip, self.target_port))
+                self.s_text.connect((self.target_ip, self.target_text_port))
+
+                break
+            except:
+                print("Couldn't connect to server")
+
+        chunk_size = 1024  # 512
+        audio_format = pyaudio.paInt16
+        channels = 1
+        rate = 20000
+
+        # initialise microphone recording
+        self.p = pyaudio.PyAudio()
+        self.playing_stream = self.p.open(format=audio_format, channels=channels, rate=rate, output=True,
+                                          frames_per_buffer=chunk_size)
+        self.recording_stream = self.p.open(format=audio_format, channels=channels, rate=rate, input=True,
+                                            frames_per_buffer=chunk_size)
+
+        print("Connected to Server")
+
+        # start threads
+        self.emotion_thread = threading.Thread(target=self.emotion_detection.start_detection).start()
+        self.receive_thread = threading.Thread(target=self.receive_server_data)
+        self.receive_thread.start()
+        self.receive_text_thread = threading.Thread(target=self.receive_server_text_data).start()
+        self.send_thread = threading.Thread(target=self.send_data_to_server).start()
+        self.send_text_thread = threading.Thread(target=self.send_text_data_to_server).start()
+
+    def receive_server_data(self):
+        while True:
+            try:
+                data = self.s.recv(1024)
+                self.playing_stream.write(data)
+            except Exception as e:
+                print(e)
+
+    def receive_server_text_data(self):
+        while True:
+            try:
+                data = self.s_text.recv(1024)
+                self.queue.put(data)
+                self.gui.process_incoming()
+            except Exception as e:
+                print(e)
+
+    def send_data_to_server(self):
+        while True:
+            try:
+                data = self.recording_stream.read(1024)
+                self.s.sendall(data)
+            except Exception as e:
+                print(e)
+
+    def send_text_data_to_server(self):
+        while True:
+            try:
+                z = self.emotion_detection.current_emotion
+                z = bytes(z, 'utf-8')
+                self.s_text.send(z)
+                time.sleep(2)
+            except Exception as e:
+                print(e)
+
+
+if __name__ == "__main__":
+    root = tkinter.Tk()
+    client = Client(root, "lucasjeske.de")
+    root.mainloop()
---
+++ emotions.py
@@ -0,0 +1,54 @@
+import numpy as np
+import cv2
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+
+
+class EmotionDetection:
+    def __init__(self):
+        self.current_emotion = "Neutral"
+        self.emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+        self.model = Sequential()
+        self.initialize_model()
+        cv2.ocl.setUseOpenCL(False) # prevents openCL usage and unnecessary logging messages
+
+    def initialize_model(self):
+        self.model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48, 48, 1)))
+        self.model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.25))
+        self.model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.25))
+        self.model.add(Flatten())
+        self.model.add(Dense(1024, activation='relu'))
+        self.model.add(Dropout(0.5))
+        self.model.add(Dense(7, activation='softmax'))
+        self.model.load_weights('model.h5')
+
+    def start_detection(self):
+        self.cap = cv2.VideoCapture(0)
+        while True:
+            # Find haar cascade to draw bounding box around face
+            ret, frame = self.cap.read()
+            if not ret:
+                break
+            facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            try:
+                faces = facecasc.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+            except Exception as e:
+                print(e)
+
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y - 50), (x + w, y + h + 10), (255, 0, 0), 2)
+                roi_gray = gray[y:y + h, x:x + w]
+                cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+                prediction = self.model.predict(cropped_img)
+                maxindex = int(np.argmax(prediction))
+                self.current_emotion = self.emotion_dict[maxindex]
+        self.cap.release()
